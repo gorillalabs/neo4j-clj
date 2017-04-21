582e3dfaf4314de4d32f7e012620d54bab0b7cf9
Dr. Christian Betz <cb@cypp.hamburg>
2017-04-21 23:42:12 +0200

    now using defquery in demo file

--- src/neo4j_clj/demo.clj
+++ src/neo4j_clj/demo.clj
@@ -1,13 +1,12 @@
 (ns neo4j-clj.demo
   (:require [neo4j-clj.core :as db]))
 
-(def create-user
-  (db/create-query "CREATE (u:User {user})"))
+(db/defquery create-user "CREATE (u:User {user})")
 
-(def get-all-users
-  (db/create-query "MATCH (u:User) RETURN u as user"))
+(db/defquery get-all-users
+             "MATCH (u:User) RETURN u as user")
 
-(def local-db (db/create-connection "bolt://localhost:7687" "neo4j" "password"))
+(def local-db (db/create-connection "bolt://localhost:7687" "neo4j" "eJD,s(3X*vcz"))
 
 (with-open [session (db/get-session local-db)]
   (create-user session {:user {:first-name "Luke" :last-name "Skywalker"}}))
