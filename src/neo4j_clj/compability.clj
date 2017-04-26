53d9d87ce1edbd28775a497a2bfb3f6573d74a64
Dr. Christian Betz <cb@cypp.hamburg>
2017-04-26 18:24:15 +0200

    added support for more node types and meta-support for labels and ids. TODO: Add tests and example!

--- src/neo4j_clj/compability.clj
+++ src/neo4j_clj/compability.clj
@@ -5,10 +5,11 @@
   (:require [clojure.walk])
   (:import (org.neo4j.driver.v1 Values)
            (org.neo4j.driver.internal InternalRecord InternalPair
-                                      InternalStatementResult)
+                                      InternalStatementResult InternalNode)
            (org.neo4j.driver.internal.value NodeValue ScalarValueAdapter
-                                            NullValue ListValue)
-           (org.neo4j.cypher.internal.javacompat ExecutionResult)))
+                                            NullValue ListValue MapValue)
+           (org.neo4j.cypher.internal.javacompat ExecutionResult)
+           (java.util Map List)))
 
 (defn clj->neo4j
   "## Convert to Neo4j
@@ -23,13 +24,28 @@
        Values/parameters))
 
 (defmulti neo4j->clj
-  "## Convert from Neo4j
+          "## Convert from Neo4j
 
-  Neo4j returns results as `StatementResults`, which contain `InternalRecords`,
-  which contain `InternalPairs` etc. Therefore, this multimethod recursively
-  calls itself with the extracted content of the data structure until we have
-  values, lists or `nil`."
-  class)
+          Neo4j returns results as `StatementResults`, which contain `InternalRecords`,
+          which contain `InternalPairs` etc. Therefore, this multimethod recursively
+          calls itself with the extracted content of the data structure until we have
+          values, lists or `nil`."
+          class)
+
+
+
+(defn transform [m]
+  (let [f (fn [[k v]]
+            [(if (string? k) (keyword k) k) (neo4j->clj v)]
+            )]
+    ;; only apply to maps
+    (clojure.walk/postwalk
+      (fn [x]
+        (if (or (map? x) (instance? Map x))
+          (with-meta (into {} (map f x))
+                     (meta x))
+          x))
+      m)))
 
 (defmethod neo4j->clj InternalStatementResult [record]
   (map neo4j->clj (iterator-seq record)))
@@ -37,17 +53,31 @@
 (defmethod neo4j->clj InternalRecord [record]
   (apply merge (map neo4j->clj (.fields record))))
 
-(defmethod neo4j->clj InternalPair [pair]
+(defmethod neo4j->clj InternalPair [^InternalPair pair]
   {(-> pair .key keyword) (-> pair .value neo4j->clj)})
 
-(defmethod neo4j->clj NodeValue [value]
-  (clojure.walk/keywordize-keys (into {} (.asMap value))))
+(defmethod neo4j->clj NodeValue [^NodeValue value]
+  (transform (into {} (.asMap value))))
 
-(defmethod neo4j->clj ScalarValueAdapter [v]
+(defmethod neo4j->clj ScalarValueAdapter [^ScalarValueAdapter v]
   (.asObject v))
 
-(defmethod neo4j->clj ListValue [l]
-  (.asList l))
+(defmethod neo4j->clj ListValue [^ListValue l]
+  (map neo4j->clj (.asList l)))
+
+(defmethod neo4j->clj MapValue [^MapValue l]
+  (transform (into {} (.asMap l))))
+
+(defmethod neo4j->clj InternalNode [^InternalNode n]
+  (with-meta (transform (into {} (.asMap n)))
+             {:labels (.labels n)
+              :id     (.id n)}))
 
 (defmethod neo4j->clj NullValue [n]
   nil)
+
+(defmethod neo4j->clj List [^List l]
+  (transform (into [] l)))
+
+(defmethod neo4j->clj :default [x]
+  x)
