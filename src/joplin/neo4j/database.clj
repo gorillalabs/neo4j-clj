5c5fe7367196a11bbe1014b6f5b59488b4e0cb51
Dr. Christian Betz <cb@cypp.hamburg>
2018-01-18 09:07:15 +0100

    fix textual representation: shouldn't show password
    this prevents accidential leaking of secrets

--- src/joplin/neo4j/database.clj
+++ src/joplin/neo4j/database.clj
@@ -49,6 +49,10 @@
     (with-connection db tx
                      (map :n.id (run-query tx "MATCH (n:migration) RETURN n.id ORDER BY n.id")))))
 
+
+(defmethod print-method Database [v ^java.io.Writer w]
+  (.write w (str "#joplin.neo4j.database.Database{:url " (.url v) ", :username " (.username v) "}")))
+
 ;; ============================================================================
 ;; Joplin interface
 
