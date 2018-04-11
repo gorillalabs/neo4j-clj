f8285cb542723f09ee1cc60d298c6e2fc774c66d
Dr. Christian Betz <cb@cypp.hamburg>
2018-04-11 11:47:09 +0200

    cleanup dependencies
    fixed #5
    updating ancient dependencies

--- project.clj
+++ project.clj
@@ -5,22 +5,23 @@
   :license {:name "MIT License"}
   :deploy-repositories [["releases" :clojars]]
 
-  :plugins [[com.roomkey/lein-v "6.2.0"]]
+  :plugins [[com.roomkey/lein-v "6.3.0"]]
   :middleware [leiningen.v/version-from-scm
                leiningen.v/add-workspace-data]
 
-  :dependencies [[org.neo4j/neo4j "3.3.1"]
-                 [org.neo4j/neo4j-cypher "3.3.1"]
-                 [org.neo4j.test/neo4j-harness "3.3.1"]
-                 [org.neo4j.driver/neo4j-java-driver "1.5.0"]
-                 [clj-time "0.14.2"]
-                 ]
+  :dependencies [[org.neo4j/neo4j "3.3.4"]
+                 [org.neo4j/neo4j-cypher "3.3.4"]
+                 [org.neo4j.driver/neo4j-java-driver "1.5.1"]
+                 [clj-time "0.14.3"]]
 
-  :profiles {:provided {:dependencies [[org.clojure/clojure "1.8.0"]
-                                       [joplin.core "0.3.10"]]}
-             :default  [:base :system :user :provided :dev #_:mirrors]
-             :dev      {:jvm-opts     ["-Dclojure.spec.check-asserts=true" "-XX:-OmitStackTraceInFastThrow"]
-                        :dependencies []}}
+  :profiles {:provided     {:dependencies [[org.clojure/clojure "1.9.0"]
+                                           [joplin.core "0.3.10"]
+                                           [org.neo4j.test/neo4j-harness "3.3.4"]]}
+             :default      [:base :system :user :provided :dev]
+             :dev          [:project/dev :profiles/dev]
+             :profiles/dev {}
+             :project/dev  {:jvm-opts     ["-Dclojure.spec.check-asserts=true" "-XX:-OmitStackTraceInFastThrow"]
+                            :dependencies []}}
 
   :scm {:name "git"
         :url  "https://github.com/gorillalabs/neo4j-clj"}
