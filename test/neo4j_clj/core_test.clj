71c08f063896d4d6dd49296b6c1147efe564d5dd
c-goetz <carlo.goetz@googlemail.com>
2019-01-23 18:56:46 +0100

    add support for IntegerValue (#13)

--- test/neo4j_clj/core_test.clj
+++ test/neo4j_clj/core_test.clj
@@ -7,7 +7,7 @@
   "CREATE (u:TestUser $user)-[:SELF {reason: \"to test\"}]->(u)")
 
 (defquery get-test-users-by-name
-  "MATCH (u:TestUser {name: $name}) RETURN u.name as name, u.role as role")
+  "MATCH (u:TestUser {name: $name}) RETURN u.name as name, u.role as role, u.age as age")
 
 (defquery get-test-users-relationship
   "MATCH (u:TestUser {name: $name})-[s:SELF]->() RETURN collect(u) as ucoll, collect(s) as scoll")
@@ -16,7 +16,7 @@
   "MATCH (u:TestUser {name: $name}) DETACH DELETE u")
 
 (def dummy-user
-  {:name "MyTestUser" :role "Dummy"})
+  {:name "MyTestUser" :role "Dummy" :age 42})
 
 (def name-lookup
   {:name (:name dummy-user)})
