6901df9313dc3e1d4d4a6cc86c695fd4098f5deb
Dr. Christian Betz <christian.betz@data42.de>
2021-04-14 20:35:19 +0200

    Cleanup example code, make sure example runs
    Example requires neo4j (e.g., in Docker).

--- example/project.clj
+++ example/project.clj
@@ -1,14 +1,11 @@
 (defproject example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [gorillalabs/neo4j-clj "4.0.2"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [gorillalabs/neo4j-clj "4.1.0"]
                  [joplin.core "0.3.11"]]
-  :profiles {:test {:dependencies [#_[org.neo4j.test/neo4j-harness "4.0.0"]]}}
+  :profiles {:test {:dependencies [#_[org.neo4j.test/neo4j-harness "4.0.0"]]}
+             :uberjar {:aot :all}}
 
-  :main ^:skip-aot example.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :main ^:skip-aot example.core)
