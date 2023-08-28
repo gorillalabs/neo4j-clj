d99bb704ad69fae0810e8161ae9aee92fae93b64
Lannie Hough <lannie.hough@protonmail.com>
2023-08-27 23:45:13 -0400

    make with-transaction work as expected, addressing issue: https://github.com/gorillalabs/neo4j-clj/issues/26

--- src/neo4j_clj/core.clj
+++ src/neo4j_clj/core.clj
@@ -76,21 +76,18 @@ to your project?" {} t)))))
 (defn get-session [^Driver connection]
   (.session (:db connection)))
 
-(defn- make-success-transaction [tx]
+(defn- make-transaction [tx]
   (proxy [org.neo4j.driver.Transaction] []
     (run
       ([q] (.run tx q))
       ([q p] (.run tx q p)))
     (commit [] (.commit tx))
     (rollback [] (.rollback tx))
-
-    ;; We only want to auto-success to ensure persistence
     (close []
-      (.commit tx)
       (.close tx))))
 
 (defn get-transaction [^Session session]
-  (make-success-transaction (.beginTransaction session)))
+  (make-transaction (.beginTransaction session)))
 
 ;; Executing cypher queries
 
@@ -127,7 +124,11 @@ to your project?" {} t)))))
 
 (defmacro with-transaction [connection tx & body]
   `(with-open [~tx (get-transaction (get-session ~connection))]
-     ~@body))
+     (try
+       ~@body
+       (.commit ~tx)
+       (finally
+         (.close ~tx)))))
 
 (defmacro with-retry [[connection tx & {:keys [max-times] :or {max-times 1000}}] & body]
   `(retry-times ~max-times
